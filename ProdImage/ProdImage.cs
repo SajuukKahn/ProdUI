8988a03662fac43c59c8a90fe92c0d72303aedd9
Michael Cannon <mcannon@pva.net>
2020-07-06 12:09:17 -0400

    Initial Commit

---
+++ ProdImage/ProdImage.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdImage
+{
+    class ProdImage : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
